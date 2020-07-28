4fe7d018f063ecd540464e92325d3f15a1ae0b34
morantaf <fall_moranta@live.fr>
2020-07-28 12:26:47 +0200

    Styling the component to display the logos in a 4*4 grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,8 +7,26 @@ import counterStrike from "../images/icons8-counter-strike-100.png";
 import finalFantasy from "../images/icons8-final-fantasy-xiv-100.png";
 import fortnite from "../images/icons8-fortnite-100.png";
 import gta from "../images/icons8-gta-5-100.png";
-
 import Card from "./Card";
+import styled from "styled-components";
+
+const Container = styled.ul`
+  margin: auto;
+  width: 650px;
+  height: 650px;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #ffffff;
+  list-style: none;
+  padding: 32px;
+  justify-content: space-between;
+`;
+
+const StyledLi = styled.li`
+  height: 100px;
+  width: 100px;
+  padding: 3%;
+`;
 
 export default function Grid() {
   const blabla = [
@@ -20,12 +38,23 @@ export default function Grid() {
     finalFantasy,
     fortnite,
     gta,
+    apex,
+    assasin,
+    warfare,
+    warzone,
+    counterStrike,
+    finalFantasy,
+    fortnite,
+    gta,
   ];
+
   return (
-    <div>
+    <Container>
       {blabla.map((bla) => (
-        <Card image={bla} />
+        <StyledLi>
+          <Card image={bla} />
+        </StyledLi>
       ))}
-    </div>
+    </Container>
   );
 }
