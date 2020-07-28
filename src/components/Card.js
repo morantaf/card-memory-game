630dbdbf8d6fe594e67291dbe6baa9d1e7c7ae4e
morantaf <fall_moranta@live.fr>
2020-07-28 15:12:05 +0200

    Styling the component, setting up the styling for the different states of the components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 5%;
+
+  padding: 5%;
+
+  &.faceUp {
+    background-color: white;
+  }
+
+  &.faceDown {
+    background-color: #555;
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 export default function Card({ image }) {
+  const [hidden, setHidden] = useState(true);
+
   return (
-    <div>
-      <img src={image} />
-    </div>
+    <Container>
+      {hidden ? (
+        <Image className="faceDown" onClick={() => setHidden(!hidden)} />
+      ) : (
+        <Image className="faceUp">
+          <img src={image} />
+        </Image>
+      )}
+    </Container>
   );
 }
