b2473c71049f064bc73e2457fbe4ad102b9ac70e
morantaf <fall_moranta@live.fr>
2020-07-29 12:57:54 +0200

    Updating the component so that the Grid handle the hidden state

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import apex from "../images/icons8-apex-legends-100.png";
-import assasin from "../images/icons8-assassins-creed-100.png";
+import assassin from "../images/icons8-assassins-creed-100.png";
 import warfare from "../images/icons8-call-of-duty-modern-warfare-100.png";
 import warzone from "../images/icons8-call-of-duty-warzone-100.png";
 import counterStrike from "../images/icons8-counter-strike-100.png";
@@ -20,32 +20,104 @@ const Container = styled.ul`
   list-style: none;
   padding: 32px;
   justify-content: space-between;
+  border-radius: 5%;
+  background: rgb(2, 0, 36);
+  background: radial-gradient(
+    circle,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(24, 141, 208, 1) 0%,
+    rgba(0, 212, 255, 1) 100%
+  );
 `;
 
-const StyledLi = styled.li`
+const StyledList = styled.li`
   height: 100px;
   width: 100px;
   padding: 3%;
 `;
 
 export default function Grid() {
-  const logoArray = [
-    apex,
-    assasin,
-    warfare,
-    warzone,
-    counterStrike,
-    finalFantasy,
-    fortnite,
-    gta,
-    apex,
-    assasin,
-    warfare,
-    warzone,
-    counterStrike,
-    finalFantasy,
-    fortnite,
-    gta,
+  const cardsList = [
+    {
+      name: "apex",
+      imgUrl: apex,
+      hidden: true,
+    },
+    {
+      name: "assassin",
+      imgUrl: assassin,
+      hidden: true,
+    },
+    {
+      name: "warfare",
+      imgUrl: warfare,
+      hidden: true,
+    },
+    {
+      name: "warzone",
+      imgUrl: warzone,
+      hidden: true,
+    },
+    {
+      name: "counter",
+      imgUrl: counterStrike,
+      hidden: true,
+    },
+    {
+      name: "final",
+      imgUrl: finalFantasy,
+      hidden: true,
+    },
+    {
+      name: "fortnite",
+      imgUrl: fortnite,
+      hidden: true,
+    },
+    {
+      name: "gta",
+      imgUrl: gta,
+      hidden: true,
+    },
+    {
+      name: "apex",
+      imgUrl: apex,
+      hidden: true,
+    },
+    {
+      name: "assassin",
+      imgUrl: assassin,
+      hidden: true,
+    },
+    {
+      name: "warfare",
+      imgUrl: warfare,
+      hidden: true,
+    },
+    {
+      name: "warzone",
+      imgUrl: warzone,
+      hidden: true,
+    },
+    {
+      name: "counter",
+      imgUrl: counterStrike,
+      hidden: true,
+    },
+    {
+      name: "final",
+      imgUrl: finalFantasy,
+      hidden: true,
+    },
+    {
+      name: "fortnite",
+      imgUrl: fortnite,
+      hidden: true,
+    },
+    {
+      name: "gta",
+      imgUrl: gta,
+      hidden: true,
+    },
   ];
 
   function shuffle(array) {
@@ -53,12 +125,9 @@ export default function Grid() {
       temporaryValue,
       randomIndex;
 
-    // While there remain elements to shuffle…
     while (currentIndex) {
-      // Pick a remaining element…
       randomIndex = Math.floor(Math.random() * currentIndex--);
 
-      // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
@@ -67,14 +136,30 @@ export default function Grid() {
     return array;
   }
 
-  const shuffledArray = shuffle(logoArray);
+  const shuffledCardsList = shuffle(cardsList);
+
+  const [displayedCards, setDisplayedCards] = useState(shuffledCardsList);
+
+  const showCard = (indexOfClickedCard) => {
+    setDisplayedCards(
+      displayedCards.map((card, index) =>
+        index === indexOfClickedCard ? { ...card, hidden: !card.hidden } : card
+      )
+    );
+  };
 
   return (
     <Container>
-      {shuffledArray.map((logo, index) => (
-        <StyledLi>
-          <Card key={index} image={logo} />
-        </StyledLi>
+      {displayedCards.map((card, index) => (
+        <StyledList>
+          <Card
+            key={index}
+            image={card.imgUrl}
+            hidden={card.hidden}
+            index={index}
+            showCard={showCard}
+          />
+        </StyledList>
       ))}
     </Container>
   );
