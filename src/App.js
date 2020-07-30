1069a082d99c18737301422b083e46e5246643db
morantaf <fall_moranta@live.fr>
2020-07-30 14:45:19 +0200

    Adding chronometer and Wrong attempts count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "./components/Grid";
+import Stats from "./components/Stats";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: auto;
+  width: 650px;
+  text-align: center;
+`;
 
 function App() {
+  const [time, setTime] = useState({ minutes: 0, seconds: 0 });
+  const [wrongAttempts, setWrongAttempts] = useState(0);
+
+  let updatedSeconds = time.seconds,
+    updatedMinutes = time.minutes;
+
+  const chrono = () => {
+    if (updatedSeconds === 60) {
+      updatedMinutes++;
+      updatedSeconds = 0;
+    }
+    updatedSeconds++;
+
+    return setTime({ minutes: updatedMinutes, seconds: updatedSeconds });
+  };
+
+  useEffect(() => {
+    chrono();
+    setInterval(chrono, 1000);
+  }, []);
+
   return (
     <div className="App">
-      <Grid />
+      <Title>Card Memory Game</Title>
+      <Stats wrongAttempts={wrongAttempts} time={time} />
+      <Grid setWrongAttempts={setWrongAttempts} wrongAttempts={wrongAttempts} />
     </div>
   );
 }
---
+++ src/components/Stats.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 650px;
+  margin: auto;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export default function Stats({ time, wrongAttempts }) {
+  return (
+    <Container>
+      <p>
+        <b>Timer</b> : {time.minutes >= 10 ? time.minutes : "0" + time.minutes}:{" "}
+        {time.seconds >= 10 ? time.seconds : "0" + time.seconds}
+      </p>
+      <p>
+        <b>Wrong attempts</b> : {wrongAttempts}
+      </p>
+    </Container>
+  );
+}
